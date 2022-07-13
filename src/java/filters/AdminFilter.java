51304ba92347ad5d2d22a28b60c2dc7802f275de
meharibrhane <meharifessehaye@gmail.com>
2022-07-13 13:07:02 -0600

    AdminFilter Web Application "AdminFilter modified commit"

--- src/java/filters/AdminFilter.java
+++ src/java/filters/AdminFilter.java
@@ -45,11 +45,11 @@ public class AdminFilter implements Filter {
 
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
     
     @Override
     public void destroy() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 }
